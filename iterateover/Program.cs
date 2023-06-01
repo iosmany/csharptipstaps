63d57e69455f83d4b1a271e2ecd29a0ad397162a
Iosmany Rodriguez Lorenzo <lorenkid@gmail.com>
2023-06-01 11:29:57 -0400

    initial commit

---
+++ iterateover/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ Analisis de iteraciones sobre colecciones con diferentes estructuras y volumenes de datos
+ 
+  - tipos de datos primitivos 
+  - tipos referencia
+  - structs 
+  - records
+ 
+ */
+
+Console.WriteLine("Hello, World!");
